7bde19ea1098534597c3cad4c3eda8c569769d92
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-30 11:22:11 -0300

    feat: Implement initial order, client, and product management features with dedicated styling and backend components.

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -10,5 +10,6 @@ router.get('/pedidos/:id', validarID, pedidosController.buscar_pedido)
 router.put('/pedidos/:id', validarID, pedidosController.atualizar_pedido)
 router.delete('/pedidos/:id', validarID, pedidosController.deletar_pedido)
 router.put('/pedidos/:id/entrega', validarID, pedidosController.entregar_pedido)
+router.get('/itens', pedidosController.listar_itens)
 
 export default router
